d88470bbc1555e1f53dfac53f961269868a265f6
Alessandro Teodori <aleteo946@gmail.com>
2022-12-27 16:40:06 +0100

    little adjustments to Main

--- Main.java
+++ Main.java
@@ -1,20 +1,17 @@
 import java.util.*;
-import java.util.Arrays;
 import java.lang.*;
 
 public class Main{
     public static void main(String[] args) {
         Scanner t = new Scanner(System.in);
 
-        System.out.println("qunti dati vuoi inserire?");
+        System.out.println("quanti dati vuoi inserire?");
         int m = t.nextInt();
         System.out.println("quante feature per dato?");
         int n = t.nextInt();
 
         double[][] dati = new double[m][n];
         Metodi.inizializzaDati(dati);
-
-        System.out.println(Arrays.deepToString(dati));
 
         double obbiettivo = 0;
         int[] cluster = new int[m];
@@ -24,6 +21,5 @@ public class Main{
         double[][] centri = new double[k][n];
         Metodi.inizializzaCluster(centri, dati);
 
-        System.out.println(Arrays.deepToString(centri));
     }
 }
